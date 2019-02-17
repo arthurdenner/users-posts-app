a554f0a0f31c9d910c1563e0c68de6fda6d060c9
Arthur Denner <arthurdenner7@gmail.com>
2019-02-17 14:32:13 -0300

    feat: Add not found page

--- src/containers/application.tsx
+++ src/containers/application.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { EmptyContent } from '../components/empty-content';
+import { ReturnToUsers } from '../components/return-to-users';
 import { Posts } from './posts';
 import { Users } from './users';
 
@@ -11,6 +13,13 @@ const Application = () => (
       <Redirect exact from="/users/:userId" to="/users/:userId/posts" />
       <Route exact path="/users" component={Users} />
       <Route exact path="/users/:userId/posts" component={Posts} />
+      <Route
+        render={() => (
+          <EmptyContent message="This page doesn't exist">
+            <ReturnToUsers />
+          </EmptyContent>
+        )}
+      />
     </Switch>
   </BrowserRouter>
 );
